6d7b082bf1881ea1277fca79f7b0e2139f87e227
Ryan Kelly <rhkelly@mit.edu>
2017-04-05 22:30:30 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# super_urop_python
---
+++ s3monitor.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import boto3
+import os
+
+def get_new_uploads(bucketname, file_set):
+	s3 = boto3.resource('s3')
+	bucket = s3.Bucket(bucketname)
+	files = [o.key for o in bucket.objects.all()]
+	new_uploads = [f for f in files if f not in file_set]
+	return new_uploads
+
+if __name__ == "__main__":
+	client = boto3.client('s3')
+	path = "/Users/rkelly/temp/"
+	bucketname = "ryankelly-superurop"
+	file_set = set(os.listdir(path))
+	while True:
+		file_names = get_new_uploads(bucketname, file_set)
+		for f in file_names:
+			client.download_file(bucketname,f,path+f)
+
+
+
