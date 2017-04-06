7751e1ce3bdf12323d3eef315232462edfd68d4d
Ryan Kelly <rhkelly@mit.edu>
2017-04-05 22:40:19 -0400

    added aws credentials

--- s3monitor.py
+++ s3monitor.py
@@ -4,14 +4,16 @@ import boto3
 import os
 
 def get_new_uploads(bucketname, file_set):
-	s3 = boto3.resource('s3')
+	s3 = boto3.resource('s3',aws_access_key_id="AKIAJNIKSIDTSZMVFFFQ",
+    aws_secret_access_key="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")
 	bucket = s3.Bucket(bucketname)
 	files = [o.key for o in bucket.objects.all()]
 	new_uploads = [f for f in files if f not in file_set]
 	return new_uploads
 
 if __name__ == "__main__":
-	client = boto3.client('s3')
+	client = boto3.client('s3',aws_access_key_id="AKIAJNIKSIDTSZMVFFFQ",
+    aws_secret_access_key="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")
 	path = "/Users/rkelly/temp/"
 	bucketname = "ryankelly-superurop"
 	file_set = set(os.listdir(path))
